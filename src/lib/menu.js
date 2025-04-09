96c3a85f9e0b566f2939d7f92a93b4d51c939470
Jaime Bolanos <jbolanos@gs1ni.org>
2025-04-09 09:25:40 -0600

    Changes in the order of the menu options, to be consistent with the process

--- src/lib/menu.js
+++ src/lib/menu.js
@@ -3,15 +3,14 @@
 export const menuOptions = [
     { label: 'Limpieza de Tablas', href: '/limpieza-tablas', roles: ['Admin'] }, 
     { label: 'Carga Datos desde Archivo Excel', href: '/carga-datos-excel', roles: ['Admin', 'Supervisor'] },
-    { label: 'Toma de Inventario', href: '/toma-inventario', roles: ['Admin', 'Supervisor', 'Inventario'] },
-    { label: 'Descargar Datos a Archivo Excel', href: '/descarga-datos-excel', roles: ['Admin', 'Supervisor'] },
     { label: 'Reporte de Resultado de Carga Excel', href: '/reporte-carga-excel', roles: ['Admin', 'Supervisor'] },
+    { label: 'Toma de Inventario', href: '/toma-inventario', roles: ['Admin', 'Supervisor', 'Inventario'] },
     { label: 'Administracion de Toma de Inventario', href: '/admin-inventario', roles: ['Admin', 'Supervisor'] },
-    { label: 'Categorias de Incidencias', href: '/categorias-incidencias', roles: ['Admin', 'Supervisor'] },
+    { label: 'Descargar Datos a Archivo Excel', href: '/descarga-datos-excel', roles: ['Admin', 'Supervisor'] },
     { label: 'Usuarios', href: '/usuarios', roles: ['Admin'] },
     { label: 'Roles', href: '/roles', roles: ['Admin'] },
+    { label: 'Categorias de Incidencias', href: '/categorias-incidencias', roles: ['Admin', 'Supervisor'] },
     { label: 'Cambiar PIN', href: '/change-pin', roles: ['Admin', 'Inventario', 'Supervisor'] }, 
-    
     { label: 'Log out', action: 'logout', roles: ['Admin', 'Inventario', 'Supervisor'] }, 
   ];
   
